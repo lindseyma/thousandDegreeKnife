db3f69aa20ae35290f6487386dc5059d2397ad6a
oreo21 <reok99@gmail.com>
2017-01-12 12:35:26 -0500

    add code that doesn't work (yet)

--- utils/zillow.py
+++ utils/zillow.py
@@ -1,6 +1,21 @@
 import urllib2, json
 
-def function(address, zipCode):
-    instream = open('keys.csv','r')
+def function():
+    instream = open('../keys.csv','r')
     content = instream.readlines()
+    instream.close()
+    zKey = content[1].split(",")[1]
+    q = "http://www.zillow.com/webservice/GetRegionChildren.htm?"
+    q += "zws-id=" + zKey
+    q += "&state=" + "wa"
+    q += "&city=" + "seattle"
+    q += "&childtype=" + "neighborhood"
+    u = urllib2.urlopen(q)
+    response = u.read()
+    data = json.loads(response)
+    return data
+
     
+if __name__ == "__main__":
+    print function()
+        
