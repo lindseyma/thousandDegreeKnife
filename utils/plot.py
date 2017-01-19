6a70dbc984ae9f960c8f24937687e858e5fbdffb
Lindsey Ma <lma2@stuy.edu>
2017-01-19 12:20:01 -0500

    lmao it returns somethreturns now

--- utils/plot.py
+++ utils/plot.py
@@ -1,11 +1,22 @@
-import urllib2, json
+import urllib, urllib2, json
 
-query = "https://data.cityofnewyork.us/resource/2fra-mtpn.json?$$app_token=TKlqxaWm34w1psikzuNcuaa2D&$select=OFNS_DESC"
+query = "https://data.cityofnewyork.us/resource/2fra-mtpn.json?$select='OFNS_DESC'"
 
 def crimeCall():
-	u = urllib2.urlopen(query)
-	json_string = u.read()
-	parsed_json = json.loads(json_string)
-	return parsed_json.keys()
-
+	try:
+		u = urllib2.urlopen(query)
+		data=json.load(u)
+		u.close()
+		for key in data:
+			return key
+		#return data
+	except urllib2.HTTPError, e:
+        	return "HTTP error: {}".format(e.code)
+  	except urllib2.URLError, e:
+       		return "Network error: {}".format(e.reason.args[1])
+	
+	##json_string = u.read()
+	##parsed_json = json.loads(json_string)
+	##return parsed_json.keys()
+	
 print crimeCall()
