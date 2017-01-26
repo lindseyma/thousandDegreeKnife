a91e9c07c4423d2c74a62cd6e968602ce191f206
lindseyma <lma2@stuy.edu>
2017-01-26 02:14:52 -0500

    hardcode

---
+++ utils/listparse2.py
@@ -0,0 +1,709 @@
+markers=[
+
+    {
+
+        'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '57 Santa Monica Ln',
+
+        'lat': 40.537131, 
+
+        'lng': -74.212247
+
+    },
+
+
+
+    {
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '23 Dongan Hills Ave',
+
+        'lat': 40.588500,
+
+        'lng': -74.101069
+
+    },
+
+
+
+    {
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '343 Willowbrook Rd',
+
+        'lat': 40.615816,
+
+        'lng': -74.145914
+
+        },
+
+
+
+    {
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '270 Glen Ave',
+
+        'lat': 42.826865,
+
+        'lng': -73.968641
+
+},
+
+
+
+{
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '7901 4th Ave APT E10',
+
+        'lat': 40.627391,
+
+        'lng': -74.026016
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '120 Avenue T # 01',
+
+        'lat': 40.598213,
+
+        'lng': -73.980866
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '3048 Brighton 7th St',
+
+        'lat': 40.578595,
+
+        'lng': -73.961157
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '14716 Rockaway Beach Blvd',
+
+        'lat': 40.569997,
+
+        'lng': -73.863189
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '27 Bartlett Pl',
+
+        'lat': 40.589119,
+
+        'lng': -73.926730
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '2034 E 71st St',
+
+        'lat': 40.620668,
+
+        'lng': -73.908762
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '971 E 87th St APT 206',
+
+        'lat': 40.637409,
+
+        'lng': -73.906601
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '1160 Rogers Ave',
+
+        'lat': 40.641621,
+
+        'lng': -73.951814
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '946 59th St APT 4B',
+
+        'lat': 40.633921,
+
+        'lng': -74.005823
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '567 8th St # 1L',
+
+        'lat': 40.665659,
+
+        'lng': -73.976777
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '107 Douglass St # 03',
+
+        'lat': 40.683076,
+
+        'lng': -73.990818
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '437 Rutland Rd',
+
+        'lat': 40.659893,
+
+        'lng': -73.946223
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '646 E 53rd St',
+
+        'lat': 40.643586,
+
+        'lng': -73.926889
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '1620 E 96th St # 1',
+
+        'lat': 40.633832,
+
+        'lng':-73.891449
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '12421 Flatlands Ave APT 5B',
+
+        'lat': 40.657533,
+
+        'lng': -73.876199
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '2308 Dean St',
+
+        'lat': 40.675667,
+
+        'lng': -73.927727
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '80 Herkimer St',
+
+        'lat': 40.679465,
+
+        'lng': -73.950824
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '225 Adams St APT 1D',
+
+        'lat': 40.697429,
+
+        'lng': -73.988221
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '739 Park Ave',
+
+        'lat': 40.770561,
+
+        'lng': -73.964103
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '749 Putnam Ave',
+
+        'lat': 40.686358,
+
+        'lng': -73.931624
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '2623 Atlantic Ave',
+
+        'lat': 40.675927,
+
+        'lng': -73.897606
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '947 Schenck Ave',
+
+        'lat': 40.657448,
+
+        'lng': -73.880828
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '155 Franklin St APT 3S',
+
+        'lat': 40.718998,
+
+        'lng': -74.008121
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '99 Avenue B # 6ED',
+
+        'lat': 40.724668,
+
+        'lng': -73.981225
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '112 Washington Pl',
+
+        'lat': 40.732459,
+
+        'lng': -74.001261
+
+},
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '230 E 15th St APT 8R',
+
+        'lat': 40.733236,
+
+        'lng': -73.985425
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '433 W 24th St APT 5C',
+
+        'lat': 40.747968,
+
+        'lng': -74.002255
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '242 E 38th St APT 4C',
+
+        'lat': 40.747410,
+
+        'lng': -73.974405
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '33 W 56th St APT 4A',
+
+        'lat': 40.763252,
+
+        'lng': -73.976129
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '333 E 53rd St APT 12B',
+
+        'lat': 40.756329,
+
+        'lng': -73.965640
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '345 E 73rd St APT 11E',
+
+        'lat': 40.768938,
+
+        'lng': -73.956056
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '230 W 78th St APT 9B',
+
+        'lat': 40.782705,
+
+        'lng': -3.980223
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '392 Central Park W APT 1V',
+
+        'lat': 40.7945233,
+
+        'lng': -73.964932
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '56-60 W 119th St # 4F',
+
+        'lat': 40.803364,
+
+        'lng': -73.947094
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '1885 Lexington Ave',
+
+        'lat': 40.799535,
+
+        'lng': -73.940654
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '346 Convent Ave',
+
+        'lat':40.824367,
+
+        'lng': -73.946772
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '712 W 175th St APT 4D',
+
+        'lat': 40.846217,
+
+        'lng':-73.939399
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '3904 49th St',
+
+        'lat': 40.748839,
+
+        'lng': -73.914893
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '2116 35th St APT 1F',
+
+        'lat': 40.776432,
+
+        'lng': -73.907359
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '3346 92nd St APT 5R',
+
+        'lat': 40.755023,
+
+        'lng': -73.876274
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '6125 98th St',
+
+        'lat': 40.733468
+
+        'lng': -73.859613
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '6630 Gray St',
+
+        'lat': 39.817271,
+
+        'lng': -105.061232
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '6945 108th St APT 7B',
+
+        'lat': 40.724030,
+
+        'lng': -73.844849
+
+},
+
+
+
+{
+
+
+
+        'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+
+        'infobox': '7012 170th St',
+
+        'lat': 40.731028,
+
+        'lng': -72.799583
+
+}
